2e695098f107171807854afeec2bf20b8547bb68
Alex <alexmartirosyan@pm.me>
2023-04-12 20:26:39 -0400

    Create messagebox.ps1
    Translate C types using Pinvoke to compile C# using PowerShell

---
+++ messagebox.ps1
@@ -0,0 +1,14 @@
+$User32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class User32 {
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int MessageBox(IntPtr hWnd, String text,
+        String caption, int options);
+}
+"@
+
+Add-Type $User32
+
+[User32]::MessageBox(0, "This is an alert", "MyBox", 0)
